259ada9d36fd1bacbde0a0ce6dc449f74c055270
Abhijit Sinha <grey.shell@gmail.com>
2020-10-15 04:03:16 -0400

    Update .travis/install_unity.sh

--- .travis/install_unity.sh
+++ .travis/install_unity.sh
@@ -4,7 +4,5 @@
 # description: install Unity - C unit testing framework
 
 mkdir lib
-# shellcheck disable=SC2164
-cd lib/
-git clone https://github.com/ThrowTheSwitch/Unity.git
+git clone https://github.com/ThrowTheSwitch/Unity.git /lib
 
