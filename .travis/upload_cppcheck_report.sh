e4957bbb923bf2b26dacaab40dbaa08754368fae
Abhijit Sinha <grey.shell@gmail.com>
2020-10-15 20:40:27 -0400

    WIP: upload issue

--- .travis/upload_cppcheck_report.sh
+++ .travis/upload_cppcheck_report.sh
@@ -4,5 +4,5 @@
 # description: upload the crash report to #dev-lib02
 
 
-curl -XPOST --data "payload={\"text\": \"\`CPPCheck\` report: $(cat test/make_build_debug/cppcheck_report.txt | \
+curl -XPOST --data "payload={\"text\": \"> \`CPPCheck\` report: $(cat test/make_build_debug/cppcheck_report.txt | \
     pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
