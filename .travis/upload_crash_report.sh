258dd249f22673d25fca5816ad947a824161f480
Abhijit Sinha <grey.shell@gmail.com>
2020-10-15 20:49:49 -0400

    Fixed the upload issue

--- .travis/upload_crash_report.sh
+++ .travis/upload_crash_report.sh
@@ -5,6 +5,6 @@
 
 for f in $(find test/make_build_debug/fuzz/ -iname "id*" | grep ".*/crashes/*")
 do
-  curl -XPOST --data "payload={\"text\": \"> Found \`crash\`: $(xxd $f | \
+  curl -XPOST --data "payload={\"text\": \"> \`Crash\` found: $(xxd $f | \
     pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
 done
