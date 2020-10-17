e465a32f14db9d99f3e8114e930b09c7069027c5
Abhijit Sinha <grey.shell@gmail.com>
2020-10-17 18:47:05 -0400

    Final fix

--- .travis/upload_cppcheck_report.sh
+++ .travis/upload_cppcheck_report.sh
@@ -4,5 +4,5 @@
 # description: upload the crash report to #dev-lib02
 
 
-curl -XPOST --data "payload={\"text\": \"> \`CPPCheck\` report: $(cat test/make_build_debug/cppcheck_report_*.txt | \
+curl -XPOST --data "payload={\"text\": \"> \`CPPCheck\` report for ${TEST_LIB}: $(cat test/make_build_debug/cppcheck_report_*.txt | \
     pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
