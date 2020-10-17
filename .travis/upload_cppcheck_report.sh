774d91601a21cda0f71468d3d96bdc2e7faa57e8
Abhijit Sinha <grey.shell@gmail.com>
2020-10-17 18:35:11 -0400

    Fixed env access

--- .travis/upload_cppcheck_report.sh
+++ .travis/upload_cppcheck_report.sh
@@ -4,5 +4,4 @@
 # description: upload the crash report to #dev-lib02
 
 
-curl -XPOST --data "payload={\"text\": \"> \`CPPCheck\` report: $(cat test/make_build_debug/cppcheck_report.txt | \
-    pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
+curl -XPOST --data "payload={\"text\": \"> \`CPPCheck\` report: $(pastebinit -b  https://paste.ubuntu.com < test/make_build_debug/cppcheck_report_${TEST_LIB})\"}" $SLACK_WEBHOOK_URL
--- .travis/upload_crash_report.sh
+++ .travis/upload_crash_report.sh
@@ -3,7 +3,7 @@
 # author: greyshell
 # description: upload the crash report to #dev-lib02
 
-for f in $(find test/make_build_debug/fuzz/ -iname "id*" | grep ".*/crashes/*")
+for f in $(find test/make_build_debug/fuzz/${TEST_LIB}/ -iname "id*" | grep ".*/crashes/*")
 do
   curl -XPOST --data "payload={\"text\": \"> \`Crash\` found: $(xxd $f | \
     pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
