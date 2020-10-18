6692b42b5c8ce490ad38532ee2abb1afd35dbf95
Abhijit Sinha <grey.shell@gmail.com>
2020-10-18 01:33:43 -0400

    Fixed repo path

--- .travis/upload_cppcheck_report.sh
+++ .travis/upload_cppcheck_report.sh
@@ -4,6 +4,6 @@
 # description: upload the crash report to #dev-lib02
 
 
-curl -XPOST --data "payload={\"text\": \"> \`CPPCheck\` report for ${TEST_LIB}: \
+curl -XPOST --data "payload={\"text\": \"> *CPPCheck* report for \`${TEST_LIB}\`: \
  $(cat test/debug/${TEST_LIB}/cppcheck_report_*.txt | \
     pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
