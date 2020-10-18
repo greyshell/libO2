01dcfab05b2f11b8140870e42bde6f697f29a0cb
Abhijit Sinha <grey.shell@gmail.com>
2020-10-18 04:59:04 -0400

    WIP:fix upload unittest

--- script/upload_unittest_report.sh
+++ script/upload_unittest_report.sh
@@ -7,8 +7,8 @@ FAILED_CASES=$(grep -w "FAIL:" test/debug/${TEST_LIB}/unittest_build/results/tes
 if [ $FAILED_CASES -gt 0 ]
 then
   # upload the report to #dev-lib02
-  curl -XPOST --data "payload={\"text\": \"> *Unittest* report for \`${TEST_LIB}\`: \
-   $(cat test/debug/${TEST_LIB}/unittest_build/results/test_${TEST_LIB}.txt | pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
+  curl -XPOST --data "payload={\"text\": \"> *Unittest* failed for \`${TEST_LIB}\` report: \
+   $(cat test/debug/${TEST_LIB}/unittest_build/results/test_${TEST_LIB}.txt | pastebinit -b https://paste.ubuntu.com).\n Skipping \`Fuzz Test\`.\"}" $SLACK_WEBHOOK_URL
   exit $FAILED_CASES
 fi
 exit $FAILED_CASES
