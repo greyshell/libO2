5ec15b1827881618c93f975a90fa03fbcf17896d
Abhijit Sinha <grey.shell@gmail.com>
2020-10-19 01:59:27 -0400

    update notification

--- script/notify_failure.sh
+++ script/notify_failure.sh
@@ -3,5 +3,5 @@
 # author: greyshell
 # description: notify the test FAIL message
 
-curl -XPOST --data "payload={\"text\": \"> :x: \`${TEST_LIB}\` :arrow_right: *FAILED* \"}" $SLACK_WEBHOOK_URL
+curl -XPOST --data "payload={\"text\": \"> :x: Test Status for \`${TEST_LIB}\`: *FAILED* \"}" $SLACK_WEBHOOK_URL
 
--- script/notify_success.sh
+++ script/notify_success.sh
@@ -3,5 +3,5 @@
 # author: greyshell
 # description: notify the test PASS message
 
-curl -XPOST --data "payload={\"text\": \"> :white_check_mark: \`${TEST_LIB}\` :arrow_right: *PASSED* \"}" $SLACK_WEBHOOK_URL
+curl -XPOST --data "payload={\"text\": \"> :white_check_mark: Test Status for \`${TEST_LIB}\`: *PASSED* \"}" $SLACK_WEBHOOK_URL
 
