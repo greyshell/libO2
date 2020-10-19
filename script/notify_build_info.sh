849764972fdd6f4836a00db82b0c6b6c32eef504
Abhijit Sinha <grey.shell@gmail.com>
2020-10-19 02:28:46 -0400

    update notification

--- script/notify_build_info.sh
+++ script/notify_build_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # author: greyshell
-# description: notify the build info
+# description: notify the build / commit info
 
 BUILD_INFO=$(git log --name-status HEAD^..HEAD | grep commit | cut -c1-14)
 curl -XPOST --data "payload={\"text\": \"> :warning: travis-ci pipeline is triggered for \`${BUILD_INFO}\` \n> Test Lib: \'${TEST_LIB}\` \n> ========================================\"}" $SLACK_WEBHOOK_URL
