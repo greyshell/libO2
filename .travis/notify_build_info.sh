09a583c6c7ebc426067377ef39a4060ce987ffbb
Abhijit Sinha <grey.shell@gmail.com>
2020-11-01 13:59:10 -0500

    test

--- .travis/notify_build_info.sh
+++ .travis/notify_build_info.sh
@@ -4,4 +4,4 @@
 # description: notify the build info
 
 BUILD_INFO=$(git log --name-status HEAD^..HEAD | grep commit | cut -c1-14)
-curl -XPOST --data "payload={\"text\": \"> :speaking_head_in_silhouette: Testing **${TEST_LIB}** on *${BUILD_INFO}* \"}" $SLACK_WEBHOOK_URL
+curl -XPOST --data "payload={\"text\": \"> :speaking_head_in_silhouette: Testing *${TEST_LIB}* on _${BUILD_INFO}_ \"}" $SLACK_WEBHOOK_URL
--- .travis/upload_cppcheck_report.sh
+++ .travis/upload_cppcheck_report.sh
@@ -7,5 +7,5 @@ FAILED_CASES=$(cat test/test_build/${TEST_LIB}/cppcheck_report_${TEST_LIB}.txt |
 if [ $FAILED_CASES -gt 1 ]
 then
   # upload the report to #dev-lib02
-  curl -XPOST --data "payload={\"text\": \"> :negative_squared_cross_mark: cppcheck failed for **${TEST_LIB}** \n> Report: $(cat test/test_build/${TEST_LIB}/cppcheck_report_*.txt | pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
+  curl -XPOST --data "payload={\"text\": \"> :negative_squared_cross_mark: cppcheck failed for *${TEST_LIB}* \n> Report: $(cat test/test_build/${TEST_LIB}/cppcheck_report_*.txt | pastebinit -b  https://paste.ubuntu.com)\"}" $SLACK_WEBHOOK_URL
 fi
